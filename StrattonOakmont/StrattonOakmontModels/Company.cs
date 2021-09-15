70983ef4ae49824e83c66f1f77f5c619b137b14c
udovenkodima7@gmail.com <udovenkodima7@gmail.com>
2021-09-15 18:22:34 +0300

    CompanyModel

---
+++ StrattonOakmont/StrattonOakmontModels/Company.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrattonOakmontModels
+{
+    class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public decimal Сapitalization { get; set; }
+    }
+}
