0bdb6159437b3bb3d4dff3cb494f9da25d883d3d
udovenkodima7@gmail.com <udovenkodima7@gmail.com>
2021-09-18 16:16:25 +0300

    PublicAdd

--- StrattonOakmont/StrattonOakmontModels/Category.cs
+++ StrattonOakmont/StrattonOakmontModels/Category.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace StrattonOakmontModels
 {
-    class Category
+    public class Category
     {
         public int Id { get; set; }
         public string CategoryName { get; set; }
--- StrattonOakmont/StrattonOakmontModels/Company.cs
+++ StrattonOakmont/StrattonOakmontModels/Company.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace StrattonOakmontModels
 {
-    class Company
+    public class Company
     {
         public int Id { get; set; }
         public string Name { get; set; }
