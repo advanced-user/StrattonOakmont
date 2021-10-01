a530e0faf3daacafb95ad12a9354fcc6ffb33ebb
DESKTOP-N47LL1N\User <udovenkodima7@gmail.com>
2021-10-01 20:56:17 +0300

    MockFinal

--- StrattonOakmont/StrattonOakmont/Pages/Security/Security.cshtml.cs
+++ StrattonOakmont/StrattonOakmont/Pages/Security/Security.cshtml.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StrattonOakmontServices;
 
 namespace StrattonOakmont.Pages.Security
 {
     public class SecurityModel : PageModel
     {
+        private readonly ISecurityRepository _db;
+        public SecurityModel(ISecurityRepository db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<StrattonOakmontModels.Security> Securities { get; set; }
+
         public void OnGet()
         {
-
+            Securities = _db.AllSecurities;
         }
     }
 }
