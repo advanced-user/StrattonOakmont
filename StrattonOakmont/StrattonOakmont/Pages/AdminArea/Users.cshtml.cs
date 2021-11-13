1e2496ac8aed4b41e60a6963c69e8206c420c355
DESKTOP-N47LL1N\User <udovenkodima7@gmail.com>
2021-11-13 18:47:36 +0200

    DeleteUserAtAdminArea

--- StrattonOakmont/StrattonOakmont/Pages/AdminArea/Users.cshtml.cs
+++ StrattonOakmont/StrattonOakmont/Pages/AdminArea/Users.cshtml.cs
@@ -1,8 +1,9 @@
-
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+using System.Collections.Generic;
 using StrattonOakmontModels;
 
 
@@ -23,5 +24,16 @@ namespace StrattonOakmont.Pages.AdminArea
         {
             Users = _userManager.Users.ToList();
         }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await _userManager.DeleteAsync(user);
+            }
+            return RedirectToPage("Users");
+        }
+       
     }
 }
