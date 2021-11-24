b22c689f31c9ece2538788fb137ffc30d761ae8c
DESKTOP-N47LL1N\User <udovenkodima7@gmail.com>
2021-11-24 17:20:29 +0200

    ModelDB

--- StrattonOakmont/StrattonOakmontServices/AppDBContext.cs
+++ StrattonOakmont/StrattonOakmontServices/AppDBContext.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using StrattonOakmontModels.Securityes;
+using StrattonOakmontModels.Securityes.Type;
 
 namespace StrattonOakmontServices
 {
@@ -16,5 +18,17 @@ namespace StrattonOakmontServices
         public DbSet<Security> Securities { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Indastrial> Indastrials{ get; set; }
+        public DbSet<SecurityInd> SecurityInds{ get; set; }
+        public DbSet<SecuritySub> SecuritySubs{ get; set; }
+        public DbSet<Bill> Bills{ get; set; }
+        public DbSet<DepositReceipts> DepositReceipts{ get; set; }
+        public DbSet<Stonk> Stonks{ get; set; }
+        public DbSet<Abligation> Abligations{ get; set; }
+        public DbSet<Forward> Forwards{ get; set; }
+        public DbSet<Futures> Futures{ get; set; }
+        public DbSet<Option> Options{ get; set; }
+        public DbSet<Wright> Wrights{ get; set; }
+
     }
 }
