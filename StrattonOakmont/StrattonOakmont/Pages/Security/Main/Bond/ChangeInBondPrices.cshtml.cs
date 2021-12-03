164ee93258d93b269166b8b0b995280b2070e2ea
Proger2k <stolbetskiy.yura@gmail.com>
2021-12-03 07:31:56 +0200

    sort companies, stocks, bonds.

--- StrattonOakmont/StrattonOakmont/Pages/Security/Main/Bond/ChangeInBondPrices.cshtml.cs
+++ StrattonOakmont/StrattonOakmont/Pages/Security/Main/Bond/ChangeInBondPrices.cshtml.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using StrattonOakmontServices;
 using StrattonOakmontServices.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StrattonOakmont.Pages.Security.Main.Bond
 {
@@ -15,14 +18,84 @@ namespace StrattonOakmont.Pages.Security.Main.Bond
             _companyRepository = companyRepository;
         }
 
-        public StrattonOakmontModels.Bond Bond { get; set; }
 
+        [BindProperty]
+        public List<StrattonOakmontModels.Securities.PriceÑhange> PriceÑhanges { get; set; }
+        
+        [BindProperty]
         public StrattonOakmontModels.Company Company { get; set; }
+
+        [BindProperty]
+        public int Sort { get; set; }
+
+        [BindProperty]
+        public int BondId { get; set; }
+
 
         public void OnGet(int companyId, int bondId)
         {
-            Bond = _bondRepository.GetBond(bondId);
+            PriceÑhanges = _bondRepository.GetBond(bondId).PriceÑhanges;
             Company = _companyRepository.FindCompany(companyId);
+            BondId = bondId;
         }
+        public void OnPostSort(string fill)
+        {
+            PriceÑhanges = _bondRepository.GetBond(BondId).PriceÑhanges;
+            Company = _companyRepository.FindCompany(Company.Id);
+
+            switch (fill)
+            {
+                case "Price":
+                    if (Sort == -1)
+                    {
+                        PriceÑhanges = PriceÑhanges.OrderBy(x => x.Price).ToList();
+                    }
+                    else
+                    {
+                        PriceÑhanges = PriceÑhanges.OrderByDescending(x => x.Price).ToList();
+                    }
+                    break;
+                case "Volume":
+                    if (Sort == -1)
+                    {
+                        PriceÑhanges = PriceÑhanges.OrderBy(x => x.Volume).ToList();
+                    }
+                    else
+                    {
+                        PriceÑhanges = PriceÑhanges.OrderByDescending(x => x.Volume).ToList();
+                    }
+                    break;
+                case "Amount":
+                    if (Sort == -1)
+                    {
+                        PriceÑhanges = PriceÑhanges.OrderBy(x => x.Amount).ToList();
+                    }
+                    else
+                    {
+                        PriceÑhanges = PriceÑhanges.OrderByDescending(x => x.Amount).ToList();
+                    }
+                    break;
+                case "Date":
+                    if (Sort == -1)
+                    {
+                        PriceÑhanges = PriceÑhanges.OrderBy(x => x.Date).ToList();
+                    }
+                    else
+                    {
+                        PriceÑhanges = PriceÑhanges.OrderByDescending(x => x.Date).ToList();
+                    }
+                    break;
+            }
+
+            if (Sort == 1)
+            {
+                Sort = -1;
+            }
+            else
+            {
+                Sort = 1;
+            }
+        }
+
     }
 }
--- StrattonOakmont/StrattonOakmont/Pages/Security/Main/Stock/ChangeInStockPrices.cshtml.cs
+++ StrattonOakmont/StrattonOakmont/Pages/Security/Main/Stock/ChangeInStockPrices.cshtml.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using StrattonOakmontServices;
 using StrattonOakmontServices.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StrattonOakmont.Pages.Security.Main.Stock
 {
@@ -16,14 +19,85 @@ namespace StrattonOakmont.Pages.Security.Main.Stock
             _companyRepository = companyRepository;
         }
 
-        public StrattonOakmontModels.Stoñk Stock { get; set; }
+        [BindProperty]
+        public List<StrattonOakmontModels.Securities.PriceÑhange> PriceÑhanges { get; set; }
+
+        [BindProperty]
 
         public StrattonOakmontModels.Company Company { get; set; }
 
+        [BindProperty]
+        public int StockId { get; set; }
+
+        [BindProperty]
+        public int Sort { get; set; }   
+
+
         public void OnGet(int companyId, int stockId)
         {
-            Stock = _stockRepository.GetStoñk(stockId);
+            PriceÑhanges = _stockRepository.GetStoñk(stockId).PriceÑhanges;
             Company = _companyRepository.FindCompany(companyId);
+            StockId = stockId; 
+            Sort = 0;
+        }
+
+        public void OnPostSort(string fill)
+        {
+            PriceÑhanges = _stockRepository.GetStoñk(StockId).PriceÑhanges;
+            Company = _companyRepository.FindCompany(Company.Id);
+
+            switch (fill)
+            {
+                case "Price":
+                    if (Sort == -1)
+                    {
+                        PriceÑhanges = PriceÑhanges.OrderBy(x => x.Price).ToList();
+                    }
+                    else
+                    {
+                        PriceÑhanges = PriceÑhanges.OrderByDescending(x => x.Price).ToList();
+                    }
+                    break;
+                case "Volume":
+                    if (Sort == -1)
+                    {
+                        PriceÑhanges = PriceÑhanges.OrderBy(x => x.Volume).ToList();
+                    }
+                    else
+                    {
+                        PriceÑhanges = PriceÑhanges.OrderByDescending(x => x.Volume).ToList();
+                    }
+                    break;
+                case "Amount":
+                    if (Sort == -1)
+                    {
+                        PriceÑhanges = PriceÑhanges.OrderBy(x => x.Amount).ToList();
+                    }
+                    else
+                    {
+                        PriceÑhanges = PriceÑhanges.OrderByDescending(x => x.Amount).ToList();
+                    }
+                    break;
+                case "Date":
+                    if (Sort == -1)
+                    {
+                        PriceÑhanges = PriceÑhanges.OrderBy(x => x.Date).ToList();
+                    }
+                    else
+                    {
+                        PriceÑhanges = PriceÑhanges.OrderByDescending(x => x.Date).ToList();
+                    }
+                    break;
+            }
+
+            if (Sort == 1)
+            {
+                Sort = -1;
+            }
+            else
+            {
+                Sort = 1;
+            }
         }
     }
 }
